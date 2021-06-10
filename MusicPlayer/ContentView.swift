7a7f8ea752d79eb145f3ed7c9c64b968e7d437ae
Marina A <25265018+MarinaBSA@users.noreply.github.com>
2021-06-11 00:32:47 +0200

    Initial Commit

---
+++ MusicPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicPlayer
+//
+//  Created by Marina Beatriz Santana de Aguiar on 11.06.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Marina Beatriz Santana de Aguiar on 11.06.21.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
